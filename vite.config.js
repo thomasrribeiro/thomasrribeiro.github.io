c9776d35ef4a32da2196ef4ef7210616be723ee0
thomasrribeiro <ribeirothomas28@gmail.com>
2025-11-14 12:32:08 -0800

    decreased font and linked Notes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        notes: resolve(__dirname, 'notes.html'),
+      }
+    }
   }
 })
